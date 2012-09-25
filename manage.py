c118d372f87ef63d15f86e5d89239ee19d846b1e
José Padilla <jpadilla@webapplicate.com>
2012-09-25 10:49:00 -0400

    Set DJANGO_SETTINGS_MODULE in manage.py from env

--- manage.py
+++ manage.py
@@ -3,7 +3,18 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+
+    ENVIRONMENT = os.getenv('ENVIRONMENT')
+
+    if ENVIRONMENT == 'STAGING':
+        settings = 'staging'
+    elif ENVIRONMENT == 'PRODUCTION':
+        settings = 'production'
+    else:
+        settings = 'development'
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+        "{{ project_name }}.settings.{settings}".format(settings=settings))
 
     from django.core.management import execute_from_command_line
 
--- project_name/settings/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-ENVIRONMENT = os.getenv('ENVIRONMENT')
-
-if ENVIRONMENT == 'STAGING':
-    from staging import *
-elif ENVIRONMENT == 'PRODUCTION':
-    from production import *
