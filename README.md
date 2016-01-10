07e01e1327218456761e52388c7ab27e594f1523
José Padilla <jpadilla@webapplicate.com>
2016-01-10 00:13:26 -0400

    Update template for Django 1.9

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# Django 1.7+ project template
+# Django 1.9+ project template
 
 [![Dependency Status](https://gemnasium.com/jpadilla/django-project-template.svg)](https://gemnasium.com/jpadilla/django-project-template)
 
-This is a simple Django 1.7+ project template with my preferred setup. Most Django project templates make way too many asumptions or are just way too complicated. I try to make the least amount of asumptions possible while still trying provide a useful setup. Most of my projects are deployed to Heroku, so this is optimized for that but is not necessary.
+This is a simple Django 1.9+ project template with my preferred setup. Most Django project templates make way too many assumptions or are just way too complicated. I try to make the least amount of assumptions possible while still trying provide a useful setup. Most of my projects are deployed to Heroku, so this is optimized for that but is not necessary.
 
-**Note**: If you're looking for the previous Django 1.4+ project template layout check out the [1.4](https://github.com/jpadilla/django-project-template/tree/1.4) branch.
+**Note**: If you're looking for the previous Django 1.7+ project template layout check out the [1.7](https://github.com/jpadilla/django-project-template/tree/1.7) branch.
 
 ## Features
 
-- Django 1.7.1
-- Development, Staging and Production settings with [django-configurations](http://django-configurations.readthedocs.org/).
-- Get value insight and debug information while on Development with [django-debug-toolbar](http://django-debug-toolbar.readthedocs.org/en/1.2.2/).
+- Django 1.9
+- Development, Staging and Production settings with [django-configurations](https://django-configurations.readthedocs.org).
+- Get value insight and debug information while on Development with [django-debug-toolbar](https://django-debug-toolbar.readthedocs.org).
 - Load environment variables from `.env` with [django-dotenv](https://github.com/jpadilla/django-dotenv).
-- Collection of custom extensions with [django-extensions](http://django-extensions.readthedocs.org/).
-- HTTPS and other security related settings on Staging and Production with [django-secure](http://django-secure.readthedocs.org/).
+- Collection of custom extensions with [django-extensions](http://django-extensions.readthedocs.org).
+- HTTPS and other security related settings on Staging and Production.
 - Procfile for running gunicorn with New Relic's Python agent.
 - PostgreSQL database support with psycopg2.
 
@@ -33,19 +33,19 @@ These are common between environments. The `ENVIRONMENT` variable loads the corr
 ENVIRONMENT='DEVELOPMENT'
 DJANGO_SECRET_KEY='dont-tell-eve'
 DJANGO_DEBUG='yes'
-DJANGO_TEMPLATE_DEBUG='yes'
 ```
 
 These settings(and their default values) are only used on staging and production environments.
 
 ```
 DJANGO_SESSION_COOKIE_SECURE='yes'
-DJANGO_SECURE_SSL_REDIRECT='yes'
-DJANGO_SECURE_HSTS_SECONDS=31536000
-DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS='yes'
-DJANGO_SECURE_FRAME_DENY='yes'
-DJANGO_SECURE_CONTENT_TYPE_NOSNIFF='yes'
 DJANGO_SECURE_BROWSER_XSS_FILTER='yes'
+DJANGO_SECURE_CONTENT_TYPE_NOSNIFF='yes'
+DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS='yes'
+DJANGO_SECURE_HSTS_SECONDS=31536000
+DJANGO_SECURE_REDIRECT_EXEMPT=''
+DJANGO_SECURE_SSL_HOST=''
+DJANGO_SECURE_SSL_REDIRECT='yes'
 DJANGO_SECURE_PROXY_SSL_HEADER='HTTP_X_FORWARDED_PROTO,https'
 ```
 
--- project_name/settings.py
+++ project_name/settings.py
@@ -22,12 +22,10 @@ class Common(Configuration):
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(False)
 
-    TEMPLATE_DEBUG = values.BooleanValue(DEBUG)
-
     ALLOWED_HOSTS = []
 
     # Application definition
-    INSTALLED_APPS = (
+    INSTALLED_APPS = [
         'django.contrib.admin',
         'django.contrib.auth',
         'django.contrib.contenttypes',
@@ -36,20 +34,36 @@ class Common(Configuration):
         'django.contrib.staticfiles',
 
         'django_extensions',
-    )
+        'debug_toolbar',
+    ]
 
-    MIDDLEWARE_CLASSES = (
-        'djangosecure.middleware.SecurityMiddleware',
+    MIDDLEWARE_CLASSES = [
+        'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    )
+    ]
 
     ROOT_URLCONF = '{{ project_name }}.urls'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
 
     WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
@@ -58,6 +72,23 @@ class Common(Configuration):
     DATABASES = values.DatabaseURLValue(
         'sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
     )
+
+    # Password validation
+    # https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#auth-password-validators
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
 
     # Internationalization
     # https://docs.djangoproject.com/en/{{ docs_version }}/topics/i18n/
@@ -82,31 +113,22 @@ class Development(Common):
     """
     DEBUG = True
 
-    TEMPLATE_DEBUG = True
-
     ALLOWED_HOSTS = []
-
-    INSTALLED_APPS = Common.INSTALLED_APPS + (
-        'debug_toolbar',
-    )
 
 
 class Staging(Common):
     """
     The in-staging settings.
     """
-    INSTALLED_APPS = Common.INSTALLED_APPS + (
-        'djangosecure',
-    )
-
-    # django-secure
+    # Security
     SESSION_COOKIE_SECURE = values.BooleanValue(True)
-    SECURE_SSL_REDIRECT = values.BooleanValue(True)
-    SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
-    SECURE_FRAME_DENY = values.BooleanValue(True)
-    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
     SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
+    SECURE_REDIRECT_EXEMPT = values.ListValue([])
+    SECURE_SSL_HOST = values.Value(None)
+    SECURE_SSL_REDIRECT = values.BooleanValue(True)
     SECURE_PROXY_SSL_HEADER = values.TupleValue(
         ('HTTP_X_FORWARDED_PROTO', 'https')
     )
