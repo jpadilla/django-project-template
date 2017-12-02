259820f3e1aca9d2b587656d3259776951ec8ee0
José Padilla <jpadilla@webapplicate.com>
2017-12-02 13:23:30 -0500

    Updates for Django 2.0

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ import dotenv
 dotenv.read_dotenv()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     ENVIRONMENT = os.getenv('ENVIRONMENT')
 
     if ENVIRONMENT == 'STAGING':
@@ -18,9 +18,15 @@ if __name__ == "__main__":
     else:
         settings = 'development'
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{ project_name }}.setting')
     os.environ.setdefault('DJANGO_CONFIGURATION', settings.title())
 
-    from configurations.management import execute_from_command_line
-
+    try:
+        from configurations.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            'Couldn\'t import Django. Are you sure it\'s installed and '
+            'available on your PYTHONPATH environment variable? Did you '
+            'forget to activate a virtual environment?'
+        ) from exc
     execute_from_command_line(sys.argv)
--- project_name/urls.py
+++ project_name/urls.py
@@ -1,14 +1,13 @@
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
