744fdf5131c9eda292f1db12d003a660655b121d
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2020-01-07 17:38:06 +0900

    updated comment url

--- project_name/settings.py
+++ project_name/settings.py
@@ -20,7 +20,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = values.SecretValue()
