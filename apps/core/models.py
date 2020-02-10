562cfd938125958a3deaaadde1d42439709b05e5
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2020-02-10 22:44:19 +0900

    Added TimeStampedModel

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,2 +1,11 @@
+from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    class Meta:
+        get_latest_by = 'updated_at'
+        ordering = ('-updated_at', '-created_at')
+        abstract = True
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
